88cfdfc2367962802102977ce02216681a29f83c
HannahLeah <Hannah.Leah@Oebb.at>
2025-02-27 13:32:59 +0100

    Add project files.

---
+++ FirstDatabase/DatabaseHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace FirstDatabase
+{
+    internal class DatabaseHandler
+    {
+        // String with attributes for connection to database
+        public string ConnectionString { get; set; }
+
+        // Constructor
+        public DatabaseHandler(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+       public ObservableCollection<Person> ExecuteSQLQueryRead(string query)
+        {
+            // Making the connection to the database
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            // opening the connection
+            conn.Open();
+
+
+            // Making a new command
+            SqlCommand cmd = conn.CreateCommand();
+            // Giving the query (SQL - command)
+            cmd.CommandText = query;
+
+            // carrying out the command with a read command
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+
+            ObservableCollection<Person> list = new ObservableCollection<Person>(); 
+
+            // Reading each line in the database
+            // as long as there are lines to read, reader(read) returns true
+
+            while (reader.Read())
+            {
+                Person tempPerson = new Person();
+                // Reading the ID
+                tempPerson.ID = reader.GetInt32(0);
+                // Reading First name
+                tempPerson.FirstName = reader.GetString(1);
+                // Reading last name
+                tempPerson.LastName = reader.GetString(2);
+                // Reading Email
+                tempPerson.Email = reader.GetString(3);
+                // Reading Birthday
+                tempPerson.Birthday = DateOnly.FromDateTime(reader.GetDateTime(4));
+
+                // Adding to the list
+                list.Add(tempPerson);
+            }
+
+            // closing the connection
+            conn.Close();
+
+            return list;
+        }
+
+           public void ExecuteSQLQueryWrite(string query)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = conn.CreateCommand();  
+                cmd.CommandText = query;
+
+                // Executes the specified query
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ FirstDatabase/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        private DateTime _birthday = DateTime.Now;
+
+        public DateTime Birthday
+        {
+            get { return _birthday; }
+            set { _birthday = value; }
+        }
+
+        public ObservableCollection<Person> PersonsObsv { get; set; }
+        private DatabaseHandler db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connection = "Data Source=HannahLeah;Initial Catalog=MyFirstDB;Integrated Security=True;Trust Server Certificate=True";
+            PersonsObsv = new ObservableCollection<Person>();
+            DataContext = this;
+           
+            db = new DatabaseHandler(connection);
+        }
+
+        private void BtnLoadDB_Click(object sender, RoutedEventArgs e)
+        {
+            PersonsObsv.Clear();
+          var temp =  db.ExecuteSQLQueryRead("SELECT * FROM Persons;");
+
+            foreach (var item in temp)
+            {
+                PersonsObsv.Add(item);
+            }
+           
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Person tempPerson = new Person();
+            tempPerson.FirstName = FirstName;
+            tempPerson.LastName = LastName;
+            tempPerson.Email = Email;
+            tempPerson.Birthday = DateOnly.FromDateTime(Birthday);
+
+            string query = $"INSERT INTO Persons(FirstName,LastName,Email,Birthday)" + $" VALUES ('{FirstName}', '{LastName}', '{Email}', "
+                + $"'{DateOnly.FromDateTime(Birthday).ToString("yyyy-MM-dd")}'); ";
+            db.ExecuteSQLQueryWrite(query);
+        }
+    }
+}
